5c2639fa6686ab658ca9908498d9ce94065293bf
Joshua Kovach <kovach.jc@gmail.com>
2016-12-09 15:33:30 -0500

    Install Timber for logging
    Right now we're just going to go with debug logging. Once we
    get further along, we can set up crash reporting with Firebase or
    Crashlytics or some other service.

--- app/src/main/java/com/collectiveidea/donuts/DonutsApp.kt
+++ app/src/main/java/com/collectiveidea/donuts/DonutsApp.kt
@@ -1,10 +1,27 @@
 package com.collectiveidea.donuts
 
 import android.app.Application
+import android.util.Log
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 class DonutsApp : Application() {
   override fun onCreate() {
     super.onCreate()
+
+    Timber.plant(loggingTree)
+  }
+
+  internal val loggingTree = if (BuildConfig.DEBUG) DebugTree() else object : Timber.Tree() {
+    override fun log(priority: Int, tag: String?, message: String?, t: Throwable?) {
+      if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+        return
+      }
+
+      TODO("Set up crash reporting")
+    }
   }
 }
+
 
