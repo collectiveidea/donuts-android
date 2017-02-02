dcd5b53b744f18bda2afa1d46fdebed5bf285da1
Joshua Kovach <kovach.jc@gmail.com>
2017-02-02 15:42:51 -0500

    Start basic view layout with tests

--- app/src/androidTest/java/com/collectiveidea/donuts/MainActivityTest.kt
+++ app/src/androidTest/java/com/collectiveidea/donuts/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.collectiveidea.donuts
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isClickable
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
@@ -22,5 +23,17 @@ import org.junit.runner.RunWith
     availabilityTextView
         .check(matches(withText("No Donuts Today :(")))
         .check(matches(isDisplayed()))
+  }
+
+  @Test fun defaultAvailabilityImage_displaysByDefault() {
+    val availabilityImage = onView(withId(R.id.donuts_availability_image))
+    availabilityImage.check(matches(isDisplayed()))
+  }
+
+  @Test fun bringingDonutsButton_isAvailableForClicking() {
+    val button = onView(withId(R.id.bringing_donuts_button))
+    button.check(matches(withText(R.string.action_announce_donuts)))
+        .check(matches(isDisplayed()))
+        .check(matches(isClickable()))
   }
 }
